f11561c18b982f37f3c34a840e02aa7b2d986bf5
Kushal Baranwal <imkushal01@gmail.com>
2023-12-23 22:29:11 +0530

    no idea wtf is going on, ill do it tmrw

--- website/src/Home/Home.jsx
+++ website/src/Home/Home.jsx
@@ -1,16 +1,31 @@
+import Card from "../Card/CardGrid";
 import Navbar from "../Navbar/Navbar";
 import "./Home.css";
+import { useCollection } from "../hooks/useCollection";
 
 export default function Home() {
+    let collection = useCollection("activities");
+    let title = [];
+    let text = [];
+    let type = [];
+    if (collection.error != null) {
+        console.log("ERROR FETCHING DOCUMENTS");
+    }
+    let array_of_documents = collection.documents;
+    if (array_of_documents != null) {
+        array_of_documents.forEach((document) => {
+            title.push(document.title);
+            text.push(document.text);
+            type.push(document.type);
+        });
+    }
     return (
         <>
             <Navbar />
             <div className="section">
-                <h2 className="sectionTitle">Chess</h2>
-                <br />
-                <div className="card">
-                    <h4>Tournament</h4>
-                    <p>Come to my tournament</p>
+                <h2 className="sectionTitle">{type}</h2>
+                <div className="cardWrapper">
+                    <Card title={title} text={text} />
                 </div>
             </div>
         </>

f12ff90d4479dee97ada2706d731bb63d431e659
Cromps <92172536+Cromps@users.noreply.github.com>
2023-12-26 08:22:09 +0530

    Navbar HTML Update

--- website/src/Navbar/Navbar.jsx
+++ website/src/Navbar/Navbar.jsx
@@ -1,49 +1,78 @@
 import {} from "react";
 import { useAuthContext } from "../hooks/useAuthContext";
 import "./Navbar.css";
-import { useState, useEffect } from "react";
-import { useLogout } from "../hooks/useLogout";
-export default function Navbar() {
-  // TO-DO: HAMBURGER MENU WHEN WIDTH < 600 PX
+import { useState, useEffect } from 'react'
 
-  let authContext = useAuthContext();
-  let user = authContext.user;
-  let authIsReady = authContext.authIsReady;
-   const {logout,isPending}=useLogout()
-  return (
-    <>
-      <div className="navbar">
-        <a className="home" href="/">
-          Home
-        </a>
-        <a className="studentForum" href="/forum">
-          Student Forum
-        </a>
-        <a className="schoolHub" href="/school">
-          School Hub
-        </a>
-        {user != null && authIsReady == true ? (
-            <>
-            {!isPending && <button onClick={logout}  >Logout</button>}
-          {isPending && <button disabled>Logging Out</button>}
-          <a className="profile" href="/profile">
-            Profile
-          </a>
-          
-          
-        
+
+export default function Navbar() {
+    // TO-DO: HAMBURGER MENU WHEN WIDTH < 600 PX
+
+    
+    let authContext = useAuthContext();
+    let user = authContext.user;
+    let authIsReady = authContext.authIsReady;
+
+    
+    return (
+        <>
+            <div className="navbar">
+                <a className="home" href="/">
+                    Home
+                </a>
+                <a className="studentForum" href="/forum">
+                    Student Forum
+                </a>
+                <a className="schoolHub" href="/school">
+                    School Hub
+                </a>
+                {user != null && authIsReady == true ? (
+                    <a className="profile" href="/profile">
+                        Profile
+                    </a>
+                ) : (
+                    <>
+                        <a className="signup" href="/signup">
+                            Sign Up
+                        </a>
+                        <a className="login" href="/login">
+                            Login
+                        </a>
+                    </>
+                )}
+            </div>
+            <label className="hamburger-menu">
+                    <input type="checkbox"/>
+            </label>
+            <aside className = "sidebar">
+                    <nav>
+                        <a className="home_burger" href="/">
+                            Home
+                        </a>
+                        <a className="studentForum_burger" href="/forum">
+                            Student Forum
+                        </a>
+                        <a className="schoolHub_burger" href="/school">
+                            School Hub
+                        </a>
+                        {user != null && authIsReady == true ? (
+                            <a className="profile_burger" href="/profile">
+                                Profile
+                            </a>
+                        ) : (
+                            <>
+                                <a className="signup_burger" href="/signup">
+                                    Sign Up
+                                </a>
+                                <a className="login_burger" href="/login">
+                                    Login
+                                </a>
+                            </>
+                        )}
+                    </nav>
+                </aside>
         </>
-        ) : (
-          <>
-            <a className="signup" href="/signup">
-              Sign Up
-            </a>
-            <a className="login" href="/login">
-              Login
-            </a>
-          </>
-        )}
-      </div>
-    </>
-  );
+
+
+
+    );
 }

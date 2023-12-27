78f0395fc6d87b85b8f9f593fe1407ea0d263f7a
Anish Vivek <anish.svek@gmail.com>
2023-12-27 18:08:05 +0530

    read ref

--- website/src/Forum/Forum.jsx
+++ website/src/Forum/Forum.jsx
@@ -1,11 +1,15 @@
 import Navbar from "../Navbar/Navbar";
 import "./Forum.css";
-
+import { useLogout } from "../hooks/useLogout";
 export default function Forum() {
+  const {logout}=useLogout()
   return (
     <>
       <Navbar />
-      <div>Forum</div>
+      <div>Forum
+
+        <button onClick={logout}>Logout</button>
+      </div>
     </>
   );
 }
--- website/src/Profile/Profile.jsx
+++ website/src/Profile/Profile.jsx
@@ -2,10 +2,58 @@ import Navbar from "../Navbar/Navbar";
 import "./Profile.css";
 import { useDocument } from "../hooks/useDocument";
 import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function Profile() {
     const { id } = useParams();
     const { document, error } = useDocument("users", id);
+    const [activity,setActivity]=useState(null)
+
+    const [searchQuery, setSearchQuery] = useState('');
+    const [currentPage, setCurrentPage] = useState(1);
+    const rowsPerPage = 10;
+
+
+  const fetchActivities = async () => {
+    if (document && document.activities) {
+      // Extract activity references
+
+      const activityDocsPromises = document.activities.map(async activity => {
+        const activityRef = activity.activity;
+        const activityDocSnapshot = await activityRef.get();
+        const activityDocData = activityDocSnapshot.data();
+        return {
+          ...activityDocData,
+          completed: activity.completed,
+          startDate:activity.startDate,
+          rating:activity.rating,
+          comment:activity.comment,
+          endDate:activity.endDate,
+        };
+      });
+
+      const activityDocs = await Promise.all(activityDocsPromises);
+      console.log(activityDocs)
+      setActivity(activityDocs)
+    }
+  };
+
+  useEffect(() => {
+    fetchActivities();
+  }, [document]);
+
+
+    const changeFilter = (newFilter) => {
+      setFilter(newFilter);
+    };
+  
+    const changeSearchQuery = (event) => {
+      setSearchQuery(event.target.value);
+    };
+    
+  
+    
     return (
         <>
             <Navbar />
@@ -17,7 +65,9 @@ export default function Profile() {
                     Profile
                     <p>Welcome: {document.displayName}</p>
                     <br></br>
-                    <p>Your friends</p>
+                    <p>Your activities</p>
+                      {document.activities && <p>Will map through</p>}
+                      {!document.activities && <p>No activities yet</p>}
                     <p>Other info.....</p>
                 </div>
             )}

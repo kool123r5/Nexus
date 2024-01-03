3d4820edaec0bd58d28b470eae7c435d97ca45e3
Kushal Baranwal <imkushal01@gmail.com>
2024-01-03 20:11:30 +0530

    fixed error detection in login

--- website/src/hooks/useLogin.js
+++ website/src/hooks/useLogin.js
@@ -1,99 +1,88 @@
 import { useState, useEffect } from "react";
 import { projectAuth, projectFirestore } from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
-import {googleProvider} from "../firebase/config"
+import { googleProvider } from "../firebase/config";
 import { useNavigate } from "react-router-dom";
 
 import "firebase/firestore";
 import { useLogout } from "./useLogout";
 
 export const useLogin = () => {
-  const [isCancelled, setIsCancelled] = useState(false);
-  const [error, setError] = useState(null);
-  const { logout } = useLogout();
-  const [isPending, setIsPending] = useState(false);
-  const { dispatch } = useAuthContext();
-  const [type, setType] = useState(null);
-  const [userFailure, setUserFailure] = useState(false);
-  const navigateTo = useNavigate();
+    const [isCancelled, setIsCancelled] = useState(false);
+    const [error, setError] = useState(false);
+    const { logout } = useLogout();
+    const [isPending, setIsPending] = useState(false);
+    const { dispatch } = useAuthContext();
+    const [type, setType] = useState(null);
+    const [userFailure, setUserFailure] = useState(false);
+    const navigateTo = useNavigate();
 
-  const login = async (email, password) => {
-    setError(null);
-    setIsPending(true);
-    setType(null);
+    const login = async (email, password) => {
+        setError(false);
+        setIsPending(true);
+        setType(null);
 
-    try {
-      // login
-      const res = await projectAuth.signInWithEmailAndPassword(email, password);
+        try {
+            // login
+            const res = await projectAuth.signInWithEmailAndPassword(email, password);
 
-      // dispatch login action
+            // dispatch login action
 
-      dispatch({ type: "LOGIN", payload: res.user });
+            dispatch({ type: "LOGIN", payload: res.user });
 
-      if (!isCancelled && !userFailure) {
-        setIsPending(false);
-        console.log(isPending);
+            if (!isCancelled && !userFailure) {
+                setIsPending(false);
+                setError(false);
+            }
+        } catch (error) {
+            if (!isCancelled) {
+                setError(JSON.parse(error.message)["error"]["message"]);
+                logout();
+                setIsPending(false);
+            }
+        }
+    };
+
+    const loginWithGoogle = async () => {
         setError(null);
-        console.log(type);
-      }
-    } catch (error) {
-      if (!isCancelled) {
-        setError(error.message);
-        logout();
-        setIsPending(false);
-      }
-    }
-  };
+        setIsPending(true);
 
+        try {
+            const res = await projectAuth.signInWithPopup(googleProvider);
 
-  const loginWithGoogle = async () => {
-    setError(null);
-    setIsPending(true);
+            const userDoc = await projectFirestore.collection("users").doc(res.user.uid).get();
+            console.log("hi");
 
-    try {
-      const res = await projectAuth.signInWithPopup(googleProvider);
-        
-      const userDoc = await projectFirestore.collection('users').doc(res.user.uid).get();
-      console.log('hi')
+            if (userDoc.exists) {
+                // dispatch login action
+                dispatch({ type: "LOGIN", payload: res.user });
+            } else {
+                // Delete the user authentication object if not registered
+                await projectAuth.currentUser.delete();
+                throw new Error("User not registered");
+            }
 
-      if (userDoc.exists) {
-        // dispatch login action
-        console.log('hi')
-        dispatch({ type: 'LOGIN', payload: res.user });
-      } else {
-        // Delete the user authentication object if not registered
-        console.log('hi')
+            navigateTo("/");
+            location.reload();
 
-        await projectAuth.currentUser.delete();
-        console.log('hi')
+            if (!isCancelled && !userFailure) {
+                setIsPending(false);
+                setError(null);
+            }
+        } catch (err) {
+            console.log(err.message);
+            if (isCancelled) {
+                setError(err.message);
 
-        throw new Error('User not registered');
-      }      
+                console.log(error);
+                setIsPending(false);
+            }
+        }
+    };
 
-      navigateTo("/");
-      location.reload();
+    // useEffect(() => {
+    //     return () => setIsCancelled(true);
+    // }, []);
 
-      if (!isCancelled && !userFailure) {
-        setIsPending(false);
-        setError(null);
-      }
-
-
-    } catch (err) {
-        console.log('hi')
-        console.log(err.message)
-      if (isCancelled) {
-        setError(err.message);
-        
-        console.log(error)
-        setIsPending(false);
-      }
-    }
-  };
-
-  useEffect(() => {
-    return () => setIsCancelled(true);
-  }, []);
-
-  return { login, loginWithGoogle, isPending, error };
+    return { login, loginWithGoogle, isPending, error };
 };

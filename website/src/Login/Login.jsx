cec2d19f926213e36e6920ae6cb9d23c0ea92b7e
Ayaan Gupta <ayaan2006gupta@gmail.com>
2024-01-16 18:58:17 +0530

    Login Page General CSS (Finetune Later)

--- website/src/Login/Login.jsx
+++ website/src/Login/Login.jsx
@@ -22,33 +22,43 @@ export default function Login() {
     return (
         <>
             <Navbar />
-            <div className="test">
-                Login
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        Email: <input type="email" onChange={(e) => setEmail(e.target.value)}></input>
-                    </label>
+            <div className="LoginBox">
+                <h1 id = "LoginText">Login</h1>
+                <div id = "Login_Container">
+                    <form onSubmit={handleSubmit}>
+                        <div id = "Login_Container2">
+                            <input className = "Login_Input" placeholder = "Email" type="email" onChange={(e) => setEmail(e.target.value)}></input>
 
-                    <label>
-                        Password: <input type="password" onChange={(e) => setPassword(e.target.value)}></input>
-                    </label>
+                            <input className = "Login_Input" placeholder = "Password" type="password" onChange={(e) => setPassword(e.target.value)}></input>
+                            <button className = "Submit_Login_Button" type="submit">Submit</button>
 
-                    <button type="submit">Submit</button>
+                        </div>
 
-                    <br />
 
-                    <button onClick={handleForgotPwClick}>Forgot password?</button>
+                        <div id = "Login_Button_Container">
 
-                    {isPending && !error && (
-                        <button className="btn" disabled>
-                            Loading...Do Not Refresh The Page
-                        </button>
-                    )}
-                    {error && <div className="error">{error}</div>}
-                    <button onClick={loginWithGoogle}>Login with Google</button>
+                            <br />
 
-                    {isPending && <p>Loading...</p>}
-                </form>
+
+                            {isPending && !error && (
+                                <button className="Submit_Login_Button" disabled>
+                                    Loading...Do Not Refresh The Page
+                                </button>
+                            )}
+                            {error && <div className="error">{error}</div>}
+                            <button className = "Submit_Login_Button" onClick={loginWithGoogle}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-google" viewBox="0 0 16 16">
+                                    <path d="M15.545 6.558a9.4 9.4 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.7 7.7 0 0 1 5.352 2.082l-2.284 2.284A4.35 4.35 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.8 4.8 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.7 3.7 0 0 0 1.599-2.431H8v-3.08z"/>
+                                </svg>
+                            </button>
+
+                            <button className = "Submit_Login_Button" onClick={handleForgotPwClick}>Forgot password?</button>
+                        </div>
+
+                        {isPending && <p>Loading...</p>}
+
+                    </form>
+                </div>
             </div>
         </>
     );

6ed66e297901e5e437a421a5077a44083b909ac2
Kushal Baranwal <imkushal01@gmail.com>
2024-01-31 20:38:00 +0530

    fixed navbar < 600px

--- website/src/Navbar/Navbar.jsx
+++ website/src/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import { useAuthContext } from "../hooks/useAuthContext";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { useViewportSize } from "@mantine/hooks";
 
 export default function Navbar() {
     let authContext = useAuthContext();
@@ -28,71 +29,78 @@ export default function Navbar() {
         navigate(`/profile/${user.uid}`);
     };
 
+    const { height, width } = useViewportSize();
+    console.log("width is" + width);
+    console.log("height is" + height);
+
     return (
         <>
-            <div className="navbar">
-                <button className="home" onClick={handleHomeClick}>
-                    Home
-                </button>
-                <button className="studentForum" onClick={handleForumClick}>
-                    Student Forum
-                </button>
-                <button className="schoolHub" onClick={handleSchoolClick}>
-                    School Hub
-                </button>
-                {user != null && authIsReady == true ? (
-                    <div className="right-buttons">
-                        <button className="profileButton" onClick={handleProfileClick}>
-                            Profile
-                        </button>
-                    </div>
-                ) : (
-                    <>
+            {width > 600 && (
+                <div className="navbar">
+                    <button className="home" onClick={handleHomeClick}>
+                        Home
+                    </button>
+                    <button className="studentForum" onClick={handleForumClick}>
+                        Student Forum
+                    </button>
+                    <button className="schoolHub" onClick={handleSchoolClick}>
+                        School Hub
+                    </button>
+                    {user != null && authIsReady && (
                         <div className="right-buttons">
-                            <button className="signup" onClick={handleSignUpClick}>
-                                Sign Up
-                            </button>
-                            <button className="login" onClick={handleLoginClick}>
-                                Login
+                            <button className="profileButton" onClick={handleProfileClick}>
+                                Profile
                             </button>
                         </div>
-                    </>
-                )}
-            </div>
-            <div id="sidebar_parent">
-                <label className="hamburger-menu">
-                    <input type="checkbox" />
-                </label>
-                <aside className="sidebar">
-                    <nav>
-                        <button className="home_burger" onClick={handleHomeClick}>
-                            Home
-                        </button>
-                        <button className="studentForum_burger" onClick={handleForumClick}>
-                            Student Forum
-                        </button>
-                        <button className="schoolHub_burger" onClick={handleSchoolClick}>
-                            School Hub
-                        </button>
-                        {user != null && authIsReady == true ? (
-
-                                <button className="profileButton" onClick={handleProfileClick}>
-                                        Profile
-                                </button>
-
-                        ) : (
-                            <>
-                                <button className="signup_burger" onClick={handleSignUpClick}>
+                    )}
+                    {user == null && authIsReady && (
+                        <>
+                            <div className="right-buttons">
+                                <button className="signup" onClick={handleSignUpClick}>
                                     Sign Up
                                 </button>
-                                <button className="login_burger" onClick={handleLoginClick}>
+                                <button className="login" onClick={handleLoginClick}>
                                     Login
                                 </button>
-                            </>
-                        )}
-                    </nav>
-                </aside>
-            </div>
+                            </div>
+                        </>
+                    )}
+                </div>
+            )}
+            {width <= 600 && (
+                <div id="sidebar_parent">
+                    <label className="hamburger-menu">
+                        <input type="checkbox" />
+                    </label>
+                    <aside className="sidebar">
+                        <nav>
+                            <button className="home_burger" onClick={handleHomeClick}>
+                                Home
+                            </button>
+                            <button className="studentForum_burger" onClick={handleForumClick}>
+                                Student Forum
+                            </button>
+                            <button className="schoolHub_burger" onClick={handleSchoolClick}>
+                                School Hub
+                            </button>
+                            {user != null && authIsReady == true ? (
+                                <button className="profileButton" onClick={handleProfileClick}>
+                                    Profile
+                                </button>
+                            ) : (
+                                <>
+                                    <button className="signup_burger" onClick={handleSignUpClick}>
+                                        Sign Up
+                                    </button>
+                                    <button className="login_burger" onClick={handleLoginClick}>
+                                        Login
+                                    </button>
+                                </>
+                            )}
+                        </nav>
+                    </aside>
+                </div>
+            )}
         </>
     );
 }

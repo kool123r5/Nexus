8742dd845f606495977f2a0c3e0a3bd6adb14f5c
Kushal Baranwal <imkushal01@gmail.com>
2024-02-26 19:17:39 +0530

    added some caching mechanism in local storage (yet to do time based)

--- website/src/Home/Home.jsx
+++ website/src/Home/Home.jsx
@@ -13,13 +13,14 @@ export default function Home() {
 
     // when we make the model, change the query to reflect the type the user would actually want to see
     const limit = 30; // the limit of how many documents to get (we don't wanna get hundreds extra when we don't need it)
-    const { documents, error } = useCollection("activities", null, null, limit);
+    const { documents, error } = useCollection("activities", null, null, limit, localStorage.getItem("ActivityDocuments"));
 
     useEffect(() => {
         if (error) {
             console.log("ERROR FETCHING DOCUMENTS");
         } else if (documents) {
             const sortedDocs = sortDocuments(documents);
+            localStorage.setItem("ActivityDocuments", JSON.stringify(sortedDocs));
             setSortedDocuments(sortedDocs);
         }
     }, [documents, error]);
@@ -27,7 +28,6 @@ export default function Home() {
     let uniqueTypeArr = null;
     if (sorted_documents) {
         uniqueTypeArr = getUniqueTypes(sorted_documents);
-        console.log(uniqueTypeArr);
     }
 
     return (
--- website/src/hooks/useCollection.js
+++ website/src/hooks/useCollection.js
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from "react";
 import { projectFirestore } from "../firebase/config";
 
-export const useCollection = (collection, _query, _orderBy, _limit) => {
+export const useCollection = (collection, _query, _orderBy, _limit, localStorageData) => {
     const [documents, setDocuments] = useState(null);
     const [error, setError] = useState(null);
 
@@ -12,40 +12,45 @@ export const useCollection = (collection, _query, _orderBy, _limit) => {
     const limit = useRef(_limit).current;
 
     useEffect(() => {
-        let ref = projectFirestore.collection(collection);
+        if (localStorageData == null || localStorageData.length == 0) {
+            let ref = projectFirestore.collection(collection);
 
-        if (query) {
-            ref = ref.where(...query);
-        }
-
-        if (orderBy) {
-            ref = ref.orderBy(...orderBy);
-        }
-
-        if (limit) {
-            ref = ref.limit(limit);
-        }
-
-        const unsubscribe = ref.onSnapshot(
-            (snapshot) => {
-                let results = [];
-                snapshot.docs.forEach((doc) => {
-                    results.push({ ...doc.data(), id: doc.id });
-                });
-
-                // update state
-                setDocuments(results);
-                setError(null);
-            },
-            (error) => {
-                console.log(error);
-                setError("could not fetch the data");
+            if (query) {
+                ref = ref.where(...query);
             }
-        );
 
-        // unsubscribe on unmount
-        return () => unsubscribe();
-    }, [collection, query, orderBy, limit]);
+            if (orderBy) {
+                ref = ref.orderBy(...orderBy);
+            }
+
+            if (limit) {
+                ref = ref.limit(limit);
+            }
+
+            const unsubscribe = ref.onSnapshot(
+                (snapshot) => {
+                    let results = [];
+                    snapshot.docs.forEach((doc) => {
+                        results.push({ ...doc.data(), id: doc.id });
+                    });
+
+                    // update state
+                    setDocuments(results);
+                    setError(null);
+                },
+                (error) => {
+                    console.log(error);
+                    setError("could not fetch the data");
+                }
+            );
+
+            // unsubscribe on unmount
+            return () => unsubscribe();
+        } else {
+            setDocuments(JSON.parse(localStorageData));
+            setError(null);
+        }
+    }, [collection, query, orderBy, limit, localStorageData]);
 
     return { documents, error };
 };

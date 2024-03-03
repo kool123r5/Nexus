459e78a0d47bc12233aebbc0d95ff31154b887f8
ArjunCodes29 <arjun051161@greenwoodhigh.edu.in>
2024-03-03 19:13:41 +0400

    rewrote merge.py added ID

--- webscraping/promptingGeminiForNewData.py
+++ webscraping/promptingGeminiForNewData.py
@@ -16,12 +16,17 @@ with open("activities_augmented_withID.json", "r", encoding="utf-8") as f:
 
 json_objects = []
 
-for i in range(0, 2):
+for i in range(2):
     activity = activities[i]
     websiteData = websiteDatas[i]
+    tags = activity["tags"]
+    title = activity["title"]
+    text = activity["text"]
+    website = activity["website"]   
+    uniqueID = activity["ID"]
     prompt = f"""
-    This is some text data extracted from the website {activity["website"]}. The title of the activity is {activity["title"]}.
-    This is a brief description of the activity - {activity["text"]}.
+    This is some text data extracted from the website {website}. The title of the activity is {title}. The Id of the activity is {uniqueID}
+    This is a brief description of the activity - {text}. 
 
     Now here is the website data we took from their website:
 
@@ -40,19 +45,23 @@ for i in range(0, 2):
     deadlineDate: String -> the last date by which applicants can apply (in format DD-MM-YYYY)
     gradeRange: String -> the range of grades which are allowed (eg: 9-12 or 6-12)
     cost: Number -> the entry fee for the event (give 0 for this if the event is free of cost)
+    ID: Number -> The ID is mentioned in the prompt simply copy that down 
 
     Make sure your final object is JSON Parseable. If you are unsure about any of the fields (if insufficient data has been provided), then replace that field with the String of unknown, no matter what the type of the field is supposed to be.
     Before you give me the object, for each of these fields, write out your reasoning before you put them in the final data object, just so you can be sure you're getting it right. After you've written all your reasoning out, then state the final data object.
+    
+
+    
     """
 
     response = model.generate_content(
         prompt, generation_config=genai.types.GenerationConfig(temperature=0.2)
     )
 
-    # print(response.text)
+    print(response.text)
     start_index = response.text.index("{")
     end_index = response.text.index("}") + 1
     json_objects.append(json.loads(response.text[start_index:end_index]))
 
 with open("extraData.json", "w") as f:
-    f.write(json.dumps(json_objects))
+   f.write(json.dumps(json_objects))

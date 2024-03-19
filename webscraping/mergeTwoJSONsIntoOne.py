6821286abde4e026cd9abba29691a43d9f041755
Kushal Baranwal <imkushal01@gmail.com>
2024-03-19 18:11:35 +0530

    merged the two arrays. unsure if this was necessary

--- webscraping/mergeTwoJSONsIntoOne.py
+++ webscraping/mergeTwoJSONsIntoOne.py
@@ -1,41 +1,54 @@
 import json
 
 
-with open("StandOutSearch_scraped.json", "r", encoding="utf-8") as f:
+with open("standOutSearchActivities.json", "r", encoding="utf-8") as f:
     standOutActivities = json.load(f)
-with open("activities_augmented_withID.json", "r", encoding="utf-8") as f:
+with open("activitiesListOG.json", "r", encoding="utf-8") as f:
     currentActivities = json.load(f)
-hugelist = []
-hugelist.extend(standOutActivities)
-hugelist.extend(currentActivities)
-
-mergedList = []
 
 
 def isDuplicate(activity1, activity2):
     if (
-        activity1["title"] == activity2["title"]
+        activity1["title"].lower() == activity2["title"].lower()
         or activity1["website"] == activity2["website"]
+        or activity1["website"] in activity2["website"]
+        or activity2["website"] in activity1["website"]
     ):
         return True
     return False
 
 
+mergedList = []
+
+
+# the first array is preferred here
 def merger(data1, data2):
-    for obj1 in data1:
-        flag = True
-        for obj2 in data2:
+    mergedList.extend(data1)
+
+    for obj2 in data2:
+        is_duplicate = False
+        for obj1 in mergedList:
             if isDuplicate(obj1, obj2):
-                flag = False
+                is_duplicate = True
                 break
-
-        if flag:
-            mergedList.append(obj1)
-    mergedList.extend(data2)
+        if not is_duplicate:
+            mergedList.append(obj2)
 
 
-def addSlashToSiteAndRemoveAllParams(website: str):
-    if website[-1] != "/":
-        new_website = website + "/"
-        return new_website
-    return website
+merger(standOutActivities, currentActivities)
+print(len(mergedList))
+deleted = len(currentActivities) + len(standOutActivities) - len(mergedList)
+print(deleted)
+
+
+def postProcessing():
+    for index, activity in enumerate(mergedList):
+        activity.pop("ID")
+        activity["id"] = index
+
+
+postProcessing()
+
+
+with open("ecListMerged.json", "w") as f:
+    json.dump(mergedList, f)

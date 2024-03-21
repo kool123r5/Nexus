bbf4453de50e13a9d63ed819c8f19c34fe4a5011
Kushal Baranwal <imkushal01@gmail.com>
2024-03-21 20:29:46 +0530

    made it easier to analyze the lowDataObj.json by adding the website link to it

--- webscraping/lowDataObjects.py
+++ webscraping/lowDataObjects.py
@@ -1,18 +1,22 @@
 import json
 
 
-with open('websiteScrapedDataVersionTwo.json', 'r', encoding='utf-8') as f:
+with open("websiteScrapedDataVersionTwo.json", "r", encoding="utf-8") as f:
     dataset = json.load(f)
 
-
+with open("activitiesListOG.json", "r") as f:
+    activityList = json.load(f)
 
 lowDataObjects = []
 
 for obj in dataset:
     data = obj["data"]
     if len(data) < 4:
+        for activity in activityList:
+            if activity["ID"] == obj["id"]:
+                obj["website"] = activity["website"]
         lowDataObjects.append(obj)
 
 print(len(lowDataObjects))
-with open("lowdataobj.json", "w") as f:
+with open("lowDataObj.json", "w") as f:
     json.dump(lowDataObjects, f, indent=4)
